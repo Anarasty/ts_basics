1226e428b6efcf543d3ec6efaf84da926cfc4b7a
Anarasty <murloc.ninja16@gmail.com>
2026-06-21 16:04:01 +0300

    type any

--- index.ts
+++ index.ts
@@ -7,9 +7,9 @@
 // number: 0, 1, 0.5, 22, -10, 4e10
 // boolean: true, false
 
-const isBirthdayData: boolean = true;
-let ageData: number = 40;
-const userNameData: string = "John";
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "John";
 
 // function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
 //   if (isBirthday) {
@@ -18,6 +18,19 @@ const userNameData: string = "John";
 //     return "Error";
 //   }
 // }
+
+let salary; //this type would be ANY
+salary = 300;
+
+const userData =
+  '{"isBirthdayData" : true, "ageData": 40, "userNameData": "John"}';
+
+const userObj: {
+  isBirthday: boolean;
+  userName: string;
+  age: number;
+} = JSON.parse(userData);
+console.log(userObj.smt());
 
 const logBrtMsg = (
   isBirthday: boolean,
