bcd2bc6c591acfca30f0e0adaaa9b9464d95365e
Anarasty <murloc.ninja16@gmail.com>
2026-06-21 12:45:29 +0300

    basic types

--- index.ts
+++ index.ts
@@ -7,3 +7,12 @@
 // number: 0, 1, 0.5, 22, -10, 4e10
 // boolean: true, false
 
+const isBirthday: boolean = true;
+let age: number;
+const userName = "John";
+
+age = 40;
+
+if (isBirthday) {
+  console.log(`Happy Birthday, ${userName.toUpperCase()}, age - ${age + 1}`);
+}
