818f7b21579c7e97e403f2c01d4b072bc08781b8
Anarasty <murloc.ninja16@gmail.com>
2026-06-22 20:03:39 +0300

    object literals and functions annotations

--- index.ts
+++ index.ts
@@ -152,26 +152,32 @@ let msg: "Hello" = "Hello";
 
 msg = "Hello";
 
-const port3000: number = 3000;
-const port3001: number = 3001;
+// const port3000: number = 3000;
+// const port3001: number = 3001;
 
-function startServer(
+const serverConfig: { protocol: "http" | "https"; port: 3000 | 3001 } = {
+  protocol: "https",
+  port: 3001,
+};
+
+const startServer: (protocol: "http" | "https", port: 3000 | 3001) => string = (
   protocol: "http" | "https",
   port: 3000 | 3001
-): "Server started" {
-  if (port === port3000 || port === port3001) {
-    console.log(`Server started on ${protocol}://server:${port}`);
-  } else {
-    console.error("Invalid port");
-  }
+): "Server started" => {
+  // if (port === port3000 || port === port3001) {
+  console.log(`Server started on ${protocol}://server:${port}`);
+  // } else {
+  // console.error("Invalid port");
+  // }
 
   return "Server started";
-}
+};
 
-startServer("https", 3001);
+// startServer("https", 3001);
+startServer(serverConfig.protocol, serverConfig.port);
 
-type AnimationTimingFunc = "ease" | "ease-out" | "ease-in"
-type AnimationID = string | number
+type AnimationTimingFunc = "ease" | "ease-out" | "ease-in";
+type AnimationID = string | number;
 
 function createAnimation(
   id: AnimationID,
@@ -183,7 +189,7 @@ function createAnimation(
   // const elem = document.querySelector(`#${id}`) as HTMLElement;
 
   // if (elem) {
-    console.log(`${animName} ${timingFunc} ${duration} ${iterCount}`);
+  console.log(`${animName} ${timingFunc} ${duration} ${iterCount}`);
   //   elem.style.animation = `${animName} ${timingFunc} ${duration} ${iterCount}`;
   // }
 }
