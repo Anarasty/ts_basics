f3d933f6e5a9d1873226247eaacc06765275b7ef
Anarasty <murloc.ninja16@gmail.com>
2026-06-24 13:34:41 +0300

    type and intersections

--- index.ts
+++ index.ts
@@ -152,28 +152,33 @@ let msg: "Hello" = "Hello";
 
 msg = "Hello";
 
-// const port3000: number = 3000;
-// const port3001: number = 3001;
+type Config = { protocol: "http" | "https"; port: 3000 | 3001 };
+type Role = { role: string };
+type ConfigWithRole = Config & Role;
 
-const serverConfig: { protocol: "http" | "https"; port: 3000 | 3001 } = {
+const serverConfig: ConfigWithRole = {
   protocol: "https",
   port: 3001,
+  role: "admin",
 };
 
-const startServer: (protocol: "http" | "https", port: 3000 | 3001) => string = (
+const backupConfig: ConfigWithRole = {
+  protocol: "http",
+  port: 3000,
+  role: "sysadmin",
+};
+
+type StartFunction = (protocol: "http" | "https", port: 3000 | 3001) => string;
+
+const startServer: StartFunction = (
   protocol: "http" | "https",
   port: 3000 | 3001
 ): "Server started" => {
-  // if (port === port3000 || port === port3001) {
   console.log(`Server started on ${protocol}://server:${port}`);
-  // } else {
-  // console.error("Invalid port");
-  // }
 
   return "Server started";
 };
 
-// startServer("https", 3001);
 startServer(serverConfig.protocol, serverConfig.port);
 
 type AnimationTimingFunc = "ease" | "ease-out" | "ease-in";
@@ -186,12 +191,7 @@ function createAnimation(
   duration: number,
   iterCount: "infinite" | number
 ): void {
-  // const elem = document.querySelector(`#${id}`) as HTMLElement;
-
-  // if (elem) {
   console.log(`${animName} ${timingFunc} ${duration} ${iterCount}`);
-  //   elem.style.animation = `${animName} ${timingFunc} ${duration} ${iterCount}`;
-  // }
 }
 
 createAnimation("id", "fadeAnim", "ease-in", 5, "infinite");
