a298211d754d6b994159fa1068864ee091b71d19
Anarasty <murloc.ninja16@gmail.com>
2026-07-30 16:27:39 +0300

    generics functions:

--- index.ts
+++ index.ts
@@ -564,16 +564,44 @@
 
 //!!!GENERICS
 
-function processingData<T>(data: T) {
-  return data;
+function processingData<T, S>(data: T[], options: S): string {
+  data.length;
+  switch (typeof data) {
+    case "string":
+      return `${data}, speed: ${options}`;
+      break;
+    case "number":
+      return `${data}, speed: ${options}`;
+      break;
+    default:
+      return "Not valid";
+  }
 }
 
-let res1 = processingData(1);
-let res2 = processingData("1");
+let res1 = processingData([1], "fast");
+let res2 = processingData(["1"], "slow");
 
 const num = 10;
 
-const res3 = processingData<number>(num);
+const res3 = processingData<number, string>([num], "slow");
+
+function processing<T>(data: T): T {
+  return data;
+}
+
+interface ProcessingFn {
+  <T>(data: T): T;
+}
+
+let newFunc: ProcessingFn = processing;
+
+interface DataSaver {
+  processing: ProcessingFn;
+}
+
+const saver: DataSaver = {
+  processing: processing,
+};
 
 interface PrintUK {
   design: number;
