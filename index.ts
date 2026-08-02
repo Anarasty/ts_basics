dc1034772667d99a577abf91e7fa6f32c4f01a66
Anarasty <murloc.ninja16@gmail.com>
2026-08-02 12:43:40 +0300

    template literal types

--- index.ts
+++ index.ts
@@ -845,36 +845,54 @@
 
 // type ExArray = ToArray<Ex | string>;
 
+// type Currencies = {
+//   usa: "usd";
+//   china?: "cny";
+//   ukraine: "uah";
+//   readonly kz: "tenge";
+// };
+
+// type CreateCustomCurr<T> = {
+//   -readonly [P in keyof T]-?: string;
+// };
+
+// type CustomCurrencies = CreateCustomCurr<Currencies>;
+
+// type ROnlyCurr = Readonly<Currencies>;
+
+// // type CustomCurrencies = {
+// //   usa: string;
+// //   china: string;
+// //   ukraine: string;
+// //   kz: string;
+// // };
+
+// type Keys = "name" | "age" | "role";
+
+// type User = {
+//   [K in Keys]: string;
+// };
+
+// const alex: User = {
+//   name: "Alex",
+//   age: "25",
+//   role: "admin",
+// };
+
 type Currencies = {
   usa: "usd";
-  china?: "cny";
   ukraine: "uah";
-  readonly kz: "tenge";
+  china: "cny";
+  kz: "tenge";
 };
 
 type CreateCustomCurr<T> = {
-  -readonly [P in keyof T]-?: string;
+  [P in keyof T as `custom${Capitalize<string & P>}`]: string;
 };
 
 type CustomCurrencies = CreateCustomCurr<Currencies>;
 
-type ROnlyCurr = Readonly<Currencies>;
+type MyAnimation = "fade" | "swipe";
+type Direction = "in" | "out";
 
-// type CustomCurrencies = {
-//   usa: string;
-//   china: string;
-//   ukraine: string;
-//   kz: string;
-// };
-
-type Keys = "name" | "age" | "role";
-
-type User = {
-  [K in Keys]: string;
-};
-
-const alex: User = {
-  name: "Alex",
-  age: "25",
-  role: "admin",
-};
+type MyNewAnimation = `${MyAnimation}${Capitalize<Direction>}`;
