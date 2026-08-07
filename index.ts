82304aadc6c544056fd08637def88cae758bb040
Anarasty <murloc.ninja16@gmail.com>
2026-08-07 13:22:09 +0300

    implements in classes

--- index.ts
+++ index.ts
@@ -1003,105 +1003,129 @@
 
 //! CLASSES
 
-class Box {
-  width: number;
-  height!: number;
-  volume: number | undefined;
-  content: string | undefined;
+// class Box {
+//   width: number;
+//   height!: number;
+//   volume: number | undefined;
+//   content: string | undefined;
 
-  constructor(width: number, volume?: number, content?: string) {
-    this.width = width;
-    this.volume = volume;
-    this.content = content;
-    this.height = 500;
-  }
+//   constructor(width: number, volume?: number, content?: string) {
+//     this.width = width;
+//     this.volume = volume;
+//     this.content = content;
+//     this.height = 500;
+//   }
 
-  calculateVolume(): void {
-    if (!this.volume) {
-      this.volume = this.width * this.height;
-      console.log(`Item volume: ${this.volume}`);
-    } else {
-      console.log(`Item volume: ${this.volume}`);
-    }
-  }
+//   calculateVolume(): void {
+//     if (!this.volume) {
+//       this.volume = this.width * this.height;
+//       console.log(`Item volume: ${this.volume}`);
+//     } else {
+//       console.log(`Item volume: ${this.volume}`);
+//     }
+//   }
 
-  checkBoxSize(transport: number): string;
-  checkBoxSize(transport: number[]): string;
-  checkBoxSize(transport: number | number[]): string {
-    if (typeof transport === "number") {
-      return transport >= this.width ? "Ok" : "Not ok";
-    } else {
-      return transport.some((t) => t >= this.width) ? "Ok" : "Not ok";
-    }
-  }
+//   checkBoxSize(transport: number): string;
+//   checkBoxSize(transport: number[]): string;
+//   checkBoxSize(transport: number | number[]): string {
+//     if (typeof transport === "number") {
+//       return transport >= this.width ? "Ok" : "Not ok";
+//     } else {
+//       return transport.some((t) => t >= this.width) ? "Ok" : "Not ok";
+//     }
+//   }
 
-  // get boxContent() {
-  //   return this.content;
-  // }
+//   // get boxContent() {
+//   //   return this.content;
+//   // }
 
-  // set boxContent(value) {
-  //   this.content = `Date: ${new Date().toTimeString()}, Content: ${value}`;
-  // }
+//   // set boxContent(value) {
+//   //   this.content = `Date: ${new Date().toTimeString()}, Content: ${value}`;
+//   // }
 
-  async contented(value: string) {
-    const date = await new Date().toTimeString();
-    this.content = `Date: ${date}, Content: ${value}`;
-    console.log(this.content);
-    // return this.content;
-  }
-}
-
-const firstBox = new Box(250);
-// console.log(firstBox.calculateVolume());
-firstBox.volume = 5000;
-// console.log(firstBox.checkBoxSize(300));
-// console.log((firstBox.boxContent = "Test"));
-// console.log(firstBox.boxContent);
-
-// class User {
-//   name!: string;
+//   async contented(value: string) {
+//     const date = await new Date().toTimeString();
+//     this.content = `Date: ${date}, Content: ${value}`;
+//     console.log(this.content);
+//     // return this.content;
+//   }
 // }
 
-// const ivan = new User();
-// ivan.name = "Ivan";
+// const firstBox = new Box(250);
+// // console.log(firstBox.calculateVolume());
+// firstBox.volume = 5000;
+// // console.log(firstBox.checkBoxSize(300));
+// // console.log((firstBox.boxContent = "Test"));
+// // console.log(firstBox.boxContent);
 
-// console.log(ivan);
+// // class User {
+// //   name!: string;
+// // }
 
-class Styles {
-  [s: string]: string | ((s: string) => boolean);
+// // const ivan = new User();
+// // ivan.name = "Ivan";
 
-  // method() { //!Error, func void
+// // console.log(ivan);
 
-  // }
+// class Styles {
+//   [s: string]: string | ((s: string) => boolean);
+
+//   // method() { //!Error, func void
+
+//   // }
+// }
+
+// const style = new Styles();
+// style.color = "red";
+// style.font = "Roboto";
+
+// class PresentBox extends Box {
+//   wrap: string;
+//   height: number = 600;
+
+//   constructor(wrap: string, width: number) {
+//     super(width);
+//     this.wrap = wrap;
+//   }
+
+//   override async contented(value: string, text?: string) {
+//     const date = await new Date().toTimeString();
+
+//     if (!text) {
+//       super.contented(value);
+//     } else {
+//       this.content = `Date: ${date}, Content: ${value}, Text: ${
+//         text ? text : "No text"
+//       }`;
+//     }
+
+//     console.log(this.content);
+//     // return this.content;
+//   }
+// }
+
+// new PresentBox("red", 500).contented("TV", "Gift");
+
+interface IUser {
+  login: string;
+  password: string;
+  token?: number;
 }
 
-const style = new Styles();
-style.color = "red";
-style.font = "Roboto";
+interface IValidation {
+  valid: boolean;
+  isValid: (data: string) => boolean;
+}
 
-class PresentBox extends Box {
-  wrap: string;
-  height: number = 600;
+class UserForm implements IUser, IValidation {
+  login!: string;
+  password!: string;
+  valid: boolean = false;
+  token!: number;
 
-  constructor(wrap: string, width: number) {
-    super(width);
-    this.wrap = wrap;
-  }
-
-  override async contented(value: string, text?: string) {
-    const date = await new Date().toTimeString();
-
-    if (!text) {
-      super.contented(value);
-    } else {
-      this.content = `Date: ${date}, Content: ${value}, Text: ${
-        text ? text : "No text"
-      }`;
-    }
-
-    console.log(this.content);
-    // return this.content;
+  isValid(login: string) {
+    return login.length > 3;
   }
 }
 
-new PresentBox("red", 500).contented("TV", "Gift");
+new UserForm().token;
